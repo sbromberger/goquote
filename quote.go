eaff4ab1a96046408b9afaf6e0f53c421eb8d047
Seth Bromberger <github@bromberger.com>
2018-07-15 21:07:18 -0700

    needs cleanup

--- quote.go
+++ quote.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -103,35 +104,51 @@ func getsymb(client *http.Client, s []string) (map[string]IEXQuote, error) {
 	return iex, nil
 }
 
-func render(iex map[string]IEXQuote) {
+func render(iex map[string]IEXQuote, sortCol int, sortDescending bool) {
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetHeader([]string{"!", "Sym", "Latest", "Open", "Close", "Chg", "%Chg", "VolM", "Time"})
 	r := tablewriter.ALIGN_RIGHT
 	table.SetColumnAlignment([]int{r, r, r, r, r, r, r, r, r})
 	var keys []string
+
 	for k := range iex {
 		keys = append(keys, k)
 	}
-	sort.Strings(keys)
-	for _, k := range keys {
-		v := iex[k]
-		ts := time.Unix(0, v.Q.AsOf*1000000)
+
+	var quoteSlice []Quote
+	for _, v := range iex {
+		quoteSlice = append(quoteSlice, v.Q)
+	}
+
+	sortFn := sortFns[sortCol]
+	sortFn(quoteSlice)
+	if sortDescending {
+		var qsrev []Quote
+		for i := len(quoteSlice) - 1; i >= 0; i-- {
+			fmt.Println("i = ", i)
+			qsrev = append(qsrev, quoteSlice[i])
+		}
+		quoteSlice = qsrev
+	}
+
+	for _, v := range quoteSlice {
+		ts := time.Unix(0, v.AsOf*1000000)
 		tz, _ := ts.Zone()
 		tsString := ts.Format("01-02 15:04:05 ") + tz
-		alertbigmove := math.Abs(v.Q.ChangePct) > bigmove
+		alertbigmove := math.Abs(v.ChangePct) > bigmove
 		rowalert := " "
 		if alertbigmove {
 			rowalert = "!"
 		}
 		row := []string{
 			rowalert,
-			v.Q.Symbol,
-			ftoa(v.Q.Latest),
-			ftoa(v.Q.Open),
-			ftoa(v.Q.Close),
-			colorizeftoa(v.Q.Change, alertbigmove),
-			colorizeftoa(v.Q.ChangePct, alertbigmove),
-			ftoa(float64(v.Q.Volume) / 1000000),
+			v.Symbol,
+			ftoa(v.Latest),
+			ftoa(v.Open),
+			ftoa(v.Close),
+			colorizeftoa(v.Change, alertbigmove),
+			colorizeftoa(v.ChangePct, alertbigmove),
+			ftoa(float64(v.Volume) / 1000000),
 			tsString,
 		}
 		table.Append(row)
@@ -139,12 +156,101 @@ func render(iex map[string]IEXQuote) {
 	table.Render()
 }
 
+func sortBySymbol(quotes []Quote) {
+	sort.Slice(quotes, func(i, j int) bool { return quotes[i].Symbol < quotes[j].Symbol })
+}
+
+func sortByLatest(quotes []Quote) {
+	sort.Slice(quotes, func(i, j int) bool { return quotes[i].Latest < quotes[j].Latest })
+}
+
+func sortByOpen(quotes []Quote) {
+	sort.Slice(quotes, func(i, j int) bool { return quotes[i].Open < quotes[j].Open })
+}
+
+func sortByClose(quotes []Quote) {
+	sort.Slice(quotes, func(i, j int) bool { return quotes[i].Close < quotes[j].Close })
+}
+
+func sortByChange(quotes []Quote) {
+	sort.Slice(quotes, func(i, j int) bool { return quotes[i].Change < quotes[j].Change })
+}
+
+func sortByChangePct(quotes []Quote) {
+	sort.Slice(quotes, func(i, j int) bool { return quotes[i].ChangePct < quotes[j].ChangePct })
+}
+
+func sortByTime(quotes []Quote) {
+	sort.Slice(quotes, func(i, j int) bool { return quotes[i].AsOf < quotes[j].AsOf })
+}
+
+func sortByVol(quotes []Quote) {
+	sort.Slice(quotes, func(i, j int) bool { return quotes[i].Volume < quotes[j].Volume })
+}
+
+var sortFns = [...]func([]Quote){
+	sortBySymbol,
+	sortByLatest,
+	sortByOpen,
+	sortByClose,
+	sortByChange,
+	sortByChangePct,
+	sortByTime,
+	sortByVol,
+}
+
 func main() {
+	var sort = flag.String("s", "+symbol", "sort by this column")
+	flag.Parse()
+
+	var fieldmap = map[string]int{
+		"symbol":    0,
+		"sym":       0,
+		"s":         0,
+		"latest":    1,
+		"l":         1,
+		"open":      2,
+		"o":         2,
+		"op":        2,
+		"close":     3,
+		"c":         3,
+		"cl":        3,
+		"change":    4,
+		"chg":       4,
+		"ch":        4,
+		"changepct": 5,
+		"chp":       5,
+		"chgp":      5,
+		"chgpct":    5,
+		"time":      6,
+		"t":         6,
+		"volume":    7,
+		"vol":       7,
+		"v":         7,
+	}
+
+	var sortDescending bool
+	var sortField string
+	if strings.HasPrefix(*sort, "+") {
+		sortDescending = false
+		sortField = string([]rune(*sort)[1:])
+	} else if strings.HasPrefix(*sort, "-") {
+		sortDescending = true
+		sortField = string([]rune(*sort)[1:])
+	} else {
+		sortDescending = true
+	}
+
+	sortCol := fieldmap[sortField]
+
+	// fmt.Println("sortCol = ", sortCol)
+	// fmt.Println("sortDescending = ", sortDescending)
+
 	client := http.Client{Timeout: time.Second * 4}
 	symbs := os.Args[1:]
 	iex, err := getsymb(&client, symbs)
 	if err != nil {
 		log.Fatal(err)
 	}
-	render(iex)
+	render(iex, sortCol, sortDescending)
 }
